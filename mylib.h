baefb299ee9b7fb023cd4091bcd0d673dd0e9c23
Namantiwari285 <namant5432@gmail.com>
2025-10-21 13:27:34 +0530

    first commit

---
+++ mylib.h
@@ -0,0 +1,8 @@
+#ifndef NAMAN_H
+#define NAMAN_H
+// Number functions
+int reverseDigits(int n);
+int isArmstrong(int num);
+int isAdams(int num);
+int isPrime(int num);
+int isPrimePalindrome(int num);
