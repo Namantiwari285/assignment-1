52cef112d351efe0e2a57992adf0f755bf9a1354
Namantiwari285 <namant5432@gmail.com>
2025-10-21 19:21:08 +0530

    Update number functions.c

--- number functions.c
+++ number functions.c
@@ -2,41 +2,59 @@
 #include <stdio.h>
 #include <math.h>
 
-
-int reverseDigits(int n) {              // number functions 
-    int r = 0;
-    while (n > 0) {
-        r = r * 10 + (n % 10);
+int reverseDigits(int n)
+{
+    int rev = 0;
+    while (n > 0)
+    {
+        int d = n % 10;
+        rev = rev * 10 + d;
         n /= 10;
     }
-    return r;
+    return rev;
 }
 
-int isArmstrong(int num) {
-    int original = num, sum = 0, digits = 0;
-    int temp = num;
-    while (temp > 0) { digits++; temp /= 10; }
-    temp = num;
-    while (temp > 0) { 
-        int d = temp % 10;
-        sum += pow(d, digits);
+int isArmstrong(int num)
+{
+    int temp = num, sum = 0, count = 0;
+    while (temp > 0)
+    {
+        count++;
         temp /= 10;
     }
-    return sum == original;
+
+    temp = num;
+    while (temp > 0)
+    {
+        int d = temp % 10;
+        sum += pow(d, count);
+        temp /= 10;
+    }
+    return (sum == num);
 }
 
-int isAdams(int num) {
-    int square = num * num;
-    return reverseDigits(square) == reverseDigits(num) * reverseDigits(num);
+int isAdams(int num)
+{
+    int sq = num * num;
+    int revSq = reverseDigits(sq);
+    int revNum = reverseDigits(num);
+    return (revSq == revNum * revNum);
 }
 
-int isPrime(int num) {
-    if (num <= 1) return 0;
-    for (int i = 2; i*i <= num; i++)
-        if (num % i == 0) return 0;
+int isPrime(int n)
+{
+    if (n <= 1)
+        return 0;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return 0;
+    }
     return 1;
 }
 
-int isPrimePalindrome(int num) {
-    return isPrime(num) && num == reverseDigits(num);
+int isPrimePalindrome(int n)
+{
+    return (isPrime(n) && n == reverseDigits(n));
 }
